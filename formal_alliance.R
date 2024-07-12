a76cba21a65d7fe2bd1e6154e00e58bb8ef280ed
pengzhaostat <86619391+pengzhaostat@users.noreply.github.com>
2024-07-12 15:57:54 -0400

    Add files via upload

--- formal_alliance.R
+++ formal_alliance.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-set.seed(2021)
+set.seed(2024)
 library('rmatio')
 library(MASS)
 library(mvtnorm)
@@ -19,13 +19,14 @@ library(ggraph)
 library(animation)
 library(Bessel)
 library(plotly)
-
+library(changepoints)
 
 file_location = dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(paste(file_location,'/function',sep=""))
 source('MP_gibbs_multi_Sigma_adaptive.R')
 source('MP_gibbs_network_sym_adaptive.R')
 source('mix_DN_adaptive_invgamma.R')
+source('change_point_WB_FFS.R')
 source('helper.R')
 
 setwd(paste(file_location,'/data',sep=""))
@@ -76,7 +77,7 @@ d=2
 
 
 MF_list = MP_binary_weighted_adaptive (Y,
-                                       gap =0.01, d=2 ,max_iter=100, global_prior = 'Cauthy')
+                                       gap =1e-2, d=d ,max_iter=100, global_prior = 'Cauthy')
 
 Xm = vector("list", T)
 for(t in 1:T){
@@ -87,6 +88,20 @@ for(t in 1:T){
 }
 
 
+M = 5 # number of random intervals for WBS
+delta = 5
+intervals = WBS.intervals(M = M, lower = 1, upper = T)
+
+FFS_result = FFS.RDPG(xhat =Xm ,betahat=MF_list$mean_beta, obs_num=T,
+                      Alpha = intervals$Alpha, Beta = intervals$Beta, delta=delta)
+
+
+### Find top 2 change point
+
+t1 = FFS_result$S[order(FFS_result$Dval,decreasing = T)[1]]
+
+t2 = FFS_result$S[order(FFS_result$Dval,decreasing = T)[2]]
+
 for ( t in 2:T){
   Xm[[t]] = t(procrustes_r(t(Xm[[t-1]]),t(Xm[[t]]))$B.transformed)
 }
@@ -94,30 +109,6 @@ for ( t in 2:T){
 
 
 
-# Xm2 = vector("list", n)
-# for(i in 1:n){
-#   Xm2 [[i]] =matrix(rep(0,T*d),nrow=T)
-#   for (t in 1:T){
-#     Xm2 [[i]][t,] = Xm [[t]][i,]
-#   }
-# }
-
-
-Xm2 = vector("list", T)
-for(t in 1:T){
-  Xm2 [[t]] =matrix(rep(0,n*d),nrow=n)
-  for (i in 1:n){
-    Xm2 [[t]][i,] = MF_list$Mean_X[[i]][t,]
-  }
-  Xm2 [[t]] = Xm2 [[t]]/sqrt(apply(Xm2[[t]]^2,1,sum))
-}
-
- heat_matrix = matrix(rep(0,n*(T-1)),ncol=T-1)
-# 
-# for (t in 2:T){
-#   heat_matrix[,t-1]= sqrt(rowSums((Xm[[t]]-Xm[[t-1]])^2))
-# }
-# heatmap(heat_matrix,Rowv=NA,Colv=NA)
 
 
 
@@ -126,33 +117,6 @@ library(tidyr)
 library(tibble)
 library(dplyr)
 library(ggExtra)
-
-
-
-
-df_plot = heat_matrix%>%
-  as_tibble() %>%
-  rowid_to_column(var="Index0") %>%
-  gather(key="t", value="Value", -1) %>%
-  mutate(year=as.numeric(gsub("V","",t))) 
-
-df_plot$year = df_plot$year+1815
-
-
-df_plot$continent = rep(data_cont,T-1)
-
-df_plot = arrange(df_plot, t,continent)
-
-df_plot$country_code = rep(1:n,T-1)
-
-# ggplot(df_plot, aes(country_code, year, fill= Value)) +
-#   geom_tile()+ scale_fill_gradient(low="white", high="red")+ theme(text = element_text(size = 16))  
-
-# df_plot$continent = rep(data_cont,T-1)
-# ggplot(df_plot, aes(country_code, year, fill= Value)) + facet_wrap(vars(continent))+
-#   geom_tile()+ scale_fill_distiller(palette = "RdPu")+ theme(text = element_text(size = 16))  
-
-
 
 
 
@@ -166,26 +130,6 @@ for (t in 1:T){
   label.plot[[t]][top_ind]=as.character(data_label[top_ind])
 }
 
-
-
-# par(mfrow=c(1,1))
-# ani.record(reset = TRUE)
-# dev.new(width=12, height=7)
-# for (t in 1:T){
-#  plot_clus_igraph(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[t]])
-#    ani.record()
-# }
-# oopts = ani.options(interval = 0.5,ani.res=600)
-# saveLatex(ani.replay(), img.name = "record_plot")
-
-
-
-
-for (t in 1:T){
-  label.plot[[t]] = rep(NA,n)
-  top_ind = which(apply(Y[[t]],1,sum)>0)
-  label.plot[[t]][top_ind]=as.character(data_label[top_ind])
-}
 
 
 # dev.new()
@@ -210,49 +154,33 @@ for (t in 1:T){
   label.plot[[t]][top_ind]=as.character(data_label[top_ind])
 }
 
-#data_name = c(1,2,16,18,20,22,26,45,48,49,59,67,92,107,115,157)
+data_name = c(1,2,16,18,20,22,25,26,27,30,45,48,49,59,67,92,107,115,157)
 
-data_name = 1:n
+#data_name = 1:n
 
-dev.new()
 
+
+### visualize t1
+par(mfrow=c(2,2))
+par(mar=rep(2,4))
+#1935 -1815 =120
+###  1935 - 1938
+plot_pre = plot_sub_preprocess(Xm,Y,t1=t1,t2=t1+3,data_label = label.plot[[t1+3]])
+for (t in t1:(t1+3)){
+  plot_clus_igraph_sub(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[t1+3]],plot_pre$axis_lim,data_name)
+}
+
+
+### visualize t2
 par(mfrow=c(2,2))
 par(mar=rep(2,4))
 #1948 -1815 =133
 ### US 1948 - 1949
-plot_pre = plot_sub_preprocess(Xm,Y,t1=133,t2=136,data_label = label.plot[[137]])
-for (t in 133:136){
-  plot_clus_igraph_sub(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[137]],plot_pre$axis_lim,data_name)
+plot_pre = plot_sub_preprocess(Xm,Y,t1=t2,t2=t2+3,data_label = label.plot[[t2+3]])
+for (t in t2:(t2+3)){
+  plot_clus_igraph_sub(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[t2+3]],plot_pre$axis_lim,data_name)
 }
 
 
-
-
-# dev.new()
-# par(mfrow=c(2,2))
-# par(mar=rep(2,4))
-# #1989 -1815 =174
-# ### US 1989 - 1992
-# #data_name = c(1:5,7:n)
-# data_name = c(1,2,16,18,20,22,26,45,48,49,59,67,92,107,115,157)
-# plot_pre = plot_sub_preprocess(Xm,Y,t1=174,t2=177,data_label = label.plot[[174]])
-# for (t in 174:177){
-#   plot_clus_igraph_sub(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[t]],plot_pre$axis_lim,data_name)
-# }
-
-
-
-
-dev.new()
-par(mfrow=c(1,2))
-par(mar=rep(2,4))
-#1989 -1815 =174
-### US 1989 - 1992
-plot_pre = plot_sub_preprocess(Xm,Y,t1=136,t2=177,data_label = label.plot[[177]])
-
-data_name = !is.na(label.plot[[177]]) | !is.na(label.plot[[136]])
-for (t in c(136,177)){
-  plot_clus_igraph_sub(Xm[[t]],Y[[t]],data_cont,t+1816-1,label.plot[[177]],plot_pre$axis_lim,data_name=which(data_name) )
-}
 
 
