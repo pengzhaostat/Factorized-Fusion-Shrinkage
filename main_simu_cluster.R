4dd79c7bb9ea4ba4b89f51370e6884bf96f3a027
pengzhaostat <86619391+pengzhaostat@users.noreply.github.com>
2024-12-05 15:45:02 -0500

    Add files via upload

---
+++ main_simu_cluster.R
@@ -0,0 +1,68 @@
+
+rm(list=ls())
+set.seed(2024)
+
+library(MASS)
+library(mvtnorm)
+library(foreach)
+library(doParallel)
+
+library(pROC)
+library(RcppEigen)
+library(RcppArmadillo)
+library(Rcpp)
+
+file_location = dirname(rstudioapi::getActiveDocumentContext()$path)
+
+
+setwd(paste(file_location,'/function',sep=""))
+source('cluster_simu_function.R')
+
+cores=detectCores()
+cl <- makeCluster(cores[1]) 
+registerDoParallel(cl)
+
+
+
+
+
+
+# Run simulation
+for (n in c(50,100,200)){
+  for (prob in c(0.99,0.95,0.9,0.8)){
+    
+    score = NULL
+    
+    # score_option1 =foreach (iter =1:50, .combine='c') %dopar% {
+    #   simu_cluster(n = n, k0=2, prob =prob)
+    # }
+    
+    cat('1,\n')
+    
+    # score_option2 =foreach (iter =1:50, .combine='c') %dopar% {
+    #   simu_cluster(n = n, k0=5, prob =prob)
+    # }
+    
+    cat('2,\n')
+    
+    
+    score_option3 =foreach (iter =1:50, .combine='c') %dopar% {
+      simu_cluster(n = n, k0=10, prob =prob)
+    }
+    
+    cat('3,\n')
+    
+    
+  #  score = c(score,score_option1,score_option2,score_option3)
+    
+    score = c(score,score_option3)
+    
+    
+    simu_name = paste(paste(paste("new_score_cluster",n,sep = ""),prob,sep = ""),'.csv',sep = "")
+    
+    write.csv(score, file = simu_name)
+  }
+}
+
+stopCluster(cl)
+
